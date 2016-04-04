141ab78168b9d85e67fd55227084c3be2be5068e
Victor Viale <somebodywas@gmail.com>
2016-04-04 11:17:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Rails VueJS ES6 Webpack boilerplate
+
+Shitty but working integration of the [VueJS official webpack-simple boilerplate](https://github.com/vuejs-templates/webpack-simple) in an empty Ruby on Rails project. 
+
+## Note
+This was made in a hurry, it has a Static Pages controller and its index action and view.
+I don't even know how it works with Sprockets and Turbolinks, so use with caution.
+
+## Gems used
+- [webpack-rails](https://github.com/mipearson/webpack-rails)
+- [vuejs-rails](https://github.com/adambutler/vuejs-rails)
---
+++ config/webpack.config.js
@@ -0,0 +1,99 @@
+// Example webpack configuration with asset fingerprinting in production.
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var StatsPlugin = require('stats-webpack-plugin');
+
+// must match config.webpack.dev_server.port
+var devServerPort = 3808;
+
+// set TARGET=production on the environment to add asset fingerprints
+var production = process.env.TARGET === 'production';
+
+var config = {
+  entry: {
+    // Sources are expected to live in $app_root/webpack
+    'application': './webpack/main.js'
+  },
+
+  output: {
+    // Build assets directly in to public/webpack/, let webpack know
+    // that all webpacked assets start with webpack/
+
+    // must match config.webpack.output_dir
+    path: path.join(__dirname, '..', 'public', 'webpack'),
+    publicPath: '/webpack/',
+
+    filename: production ? '[name]-[chunkhash].js' : '[name].js'
+  },
+
+  resolve: {
+    root: path.join(__dirname, '..', 'webpack')
+  },
+
+  plugins: [
+    // must match config.webpack.manifest_filename
+    new StatsPlugin('manifest.json', {
+      // We only need assetsByChunkName
+      chunkModules: false,
+      source: false,
+      chunks: false,
+      modules: false,
+      assets: true
+    })],
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.html$/,
+        loader: 'vue-html'
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  }
+};
+
+if (production) {
+  config.plugins.push(
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: { warnings: false },
+      sourceMap: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify('production') }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin()
+  );
+} else {
+  config.devServer = {
+    port: devServerPort,
+    headers: { 'Access-Control-Allow-Origin': '*' }
+  };
+  config.output.publicPath = '//localhost:' + devServerPort + '/webpack/';
+  // Source maps
+  config.devtool = 'cheap-module-eval-source-map';
+}
+
+module.exports = config;
